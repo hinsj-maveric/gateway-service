733a1775fda819bd92c15e603cf10195b560e234
Hins Jain <hinsj@maveric-systems.com>
2023-02-01 01:49:46 +0530

    Added all the services in configuration

--- src/main/java/com/maveric/gatewayservice/configuration/GatewayConfiguration.java
+++ src/main/java/com/maveric/gatewayservice/configuration/GatewayConfiguration.java
@@ -2,8 +2,7 @@ package com.maveric.gatewayservice.configuration;
 
 import com.maveric.gatewayservice.filter.AuthenticationPreFilter;
 //import com.maveric.gatewayservice.filter.JwtAuthFilter;
-import com.maveric.gatewayservice.properties.AccountProperties;
-import com.maveric.gatewayservice.properties.BalanceProperties;
+import com.maveric.gatewayservice.properties.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -38,10 +37,18 @@ public class GatewayConfiguration {
 
 
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, AuthenticationPreFilter authenticationPreFilter, BalanceProperties balanceProperties,
-                                      AccountProperties accountProperties) {
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, AuthenticationPreFilter authenticationPreFilter,
+                                      BalanceProperties balanceProperties,
+                                      AccountProperties accountProperties, UserProperties userProperties,
+                                      TransactionProperties transactionProperties,
+                                      AuthProperties authProperties) {
         return builder.routes()
 
+                .route(routeToBalance -> routeToBalance.path(userProperties.getPath())
+                        .filters(f ->
+                                f.filter(authenticationPreFilter.apply(
+                                        new AuthenticationPreFilter.Config())))
+                        .uri(userProperties.getUri()))
                 .route(routeToBalance -> routeToBalance.path(balanceProperties.getPath())
                         .filters(f ->
                                 f.filter(authenticationPreFilter.apply(
@@ -52,10 +59,13 @@ public class GatewayConfiguration {
                                 f.filter(authenticationPreFilter.apply(
                                         new AuthenticationPreFilter.Config())))
                         .uri(accountProperties.getUri()))
-
-
+                .route(routeToBalance -> routeToBalance.path(transactionProperties.getPath())
+                        .filters(f ->
+                                f.filter(authenticationPreFilter.apply(
+                                        new AuthenticationPreFilter.Config())))
+                        .uri(transactionProperties.getUri()))
+                .route(routeToAccount -> routeToAccount.path(authProperties.getPath())
+                        .uri(authProperties.getUri()))
                 .build();
     }
-    }
-
-
+}
