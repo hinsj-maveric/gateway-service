51b04854bfcebe7c5706383a37a3bc4e06fdf4ca
Hins Jain <hinsj@maveric-systems.com>
2023-02-20 19:59:24 +0530

    Changed configuration for docker container

--- src/main/java/com/maveric/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/maveric/gatewayservice/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.maveric.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -12,13 +14,26 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
-@EnableEurekaClient
-
+@EnableDiscoveryClient
+@EnableFeignClients
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+		corsConfig.addAllowedHeader("*");
+		corsConfig.addExposedHeader("Authorization");
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return new CorsWebFilter(source);
 	}
 
 
--- src/main/java/com/maveric/gatewayservice/configuration/GatewayConfiguration.java
+++ src/main/java/com/maveric/gatewayservice/configuration/GatewayConfiguration.java
@@ -20,23 +20,6 @@ import java.util.Collections;
 public class GatewayConfiguration {
 
     @Bean
-    public CorsWebFilter corsWebFilter() {
-
-        final CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addExposedHeader("Authorization");
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/v1/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
-
-
-    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, AuthenticationPreFilter authenticationPreFilter,
                                       BalanceProperties balanceProperties,
                                       AccountProperties accountProperties, UserProperties userProperties,
